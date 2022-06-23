54aeaf628ce2e52a33b0b54bdfe10cc588d2aa54
bruce <bruceliu1107@gmail.com>
2022-06-22 22:18:10 -0400

    findPosition function

--- hand-tracking/HandTrackingModule.py
+++ hand-tracking/HandTrackingModule.py
@@ -13,31 +13,38 @@ class handDetector():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands,1 , self.detectionCon, self.trackCon) # model_complexity=1,
         self.mpDraw = mp.solutions.drawing_utils # we use this to draw lines and dots
 
+    # return the image with hand point and hand connections
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # convert to RGB first because our package only work with RGB
 
-        results = self.hands.process(imgRGB)  # after this, all we need is to extract the results and use them
+        self.results = self.hands.process(imgRGB)  # after this, all we need is to extract the results and use them
 
-        if results.multi_hand_landmarks:  # if hands are found
-            for handLms in results.multi_hand_landmarks:  # we loop through the land landmark
+        if self.results.multi_hand_landmarks:  # if hands are found
+            for handLms in self.results.multi_hand_landmarks:  # we loop through the land landmark
 
-                # for id, lm in enumerate(
-                        # handLms.landmark):  # get landmark and index ; 0 = bottom middle one , 4 is a tip
-                    # print(id, lm); landmark has x, y and z postion, we use x and y to find the position
-                    # x: 0.7550578713417053
-                    # y: 0.2370118796825409
-                    # z: -0.008621162734925747
-                    # these gives us the ratio, we multiply it by width and height, then we can get the pixel
-                    # h, w, c = img.shape  # get width, height, and channel.
-                    # cx, cy = int(lm.x * w), int(lm.y * h)  # find the pisition of the center ( for all 21 values )
-                    # print(id, cx, cy)
-
-                    # if id == 0:  # index 0 of the hand ( bottom of the palm ), 4 is the tip of thumb
-                        # cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)  # 25 is the size of the circle
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms,
                                       self.mpHands.HAND_CONNECTIONS)  # we are displaying the BGR image, not the RGB one, and here we are drawing the hand dows
         return img
+
+    # return the position of 21 points in the hand as a list. such as [0, 25, 265].... as index 0 is at x=25, y=265
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmList = []
+
+        if self.results.multi_hand_landmarks:  # if hands are found
+            myHand = self.results.multi_hand_landmarks[handNo] #get the desired hand number's landmark
+
+            for id, lm in enumerate(myHand.landmark):  # within that hand, get all the landmarks
+                h, w, c = img.shape  # get width, height, and channel.
+                cx, cy = int(lm.x * w), int(lm.y * h)  # find the pisition of the center ( for all 21 values )
+                # print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)  # 25 is the size of the circle
+
+        return lmList
+
 
 def main():
     pTime = 0
@@ -47,6 +54,9 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
 
         cTime = time.time()  # current time
         fps = 1 / (cTime - pTime)
@@ -57,7 +67,6 @@ def main():
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
-
 
 if __name__ == "__main__": # we are going to write in main to show what this module can do
     main()
